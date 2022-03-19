da98a8ad51988d5479f91d844f94ee04c0a49b27
Karen Mayumi Hirayama <karenm.hirayama@gmail.com>
2022-03-19 16:27:25 -0300

    Create components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import './App.css';
+import { BitcoinChart } from './components/BitcoinChart/BitcoinChart';
+import { ProfitCalculator } from './components/Calculation/ProfitCalculator/ProfitCalculator';
+import { Footer } from './components/Footer/Footer';
+import { Header } from './components/Header/Header';
 
 function App() {
   return (
     <>
+      <Header />
+      <BitcoinChart />
+      <ProfitCalculator />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Calculation/ProfitCalculator/ProfitCalculator.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const ProfitCalculator = () => {
+  return (
+    <div>ProfitCalculator</div>
+  )
+}
